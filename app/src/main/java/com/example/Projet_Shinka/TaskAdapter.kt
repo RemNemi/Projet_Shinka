d4a88129235648e1d17c65240e623f5e400903dc
TORNADOUDIDOU <tornadou59224@gmail.com>
2024-03-22 16:45:03 +0100

    front head taches test

---
+++ app/src/main/java/com/example/Projet_Shinka/TaskAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.Projet_Shinka
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.Projet_Shinka.model.Task
+
+class TaskAdapter(private val tasks: List<Task>) : RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textViewTitle: TextView = view.findViewById(R.id.textViewTitle)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val task = tasks[position]
+        holder.textViewTitle.text = task.title
+    }
+
+    override fun getItemCount() = tasks.size
+}
